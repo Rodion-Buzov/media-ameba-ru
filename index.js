fca922534b580cb9ebc63b4ce9f007ea1d50e978
Rodion-Buzov <megisant18@gmail.com>
2024-07-29 18:17:35 +0300

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const cowsay = require("cowsay")
+
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('frontend'));
+
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+    response.render("index");
+});
+
+
+
+// подрубаем просшулку
+app.listen(3000);
+console.log(cowsay.say({
+    text : "Сервер подрубил прослушку",
+    e : "oO",
+    T : "U "
+}));
